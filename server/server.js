6c9752e0ace149c236aa48b4b969761c12ba5743
pravalikadande <dandeprvalika160@gmail.com>
2026-07-07 13:56:36 +0530

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.get("/", (req, res) => {
   res.send("Lost and Found API is Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

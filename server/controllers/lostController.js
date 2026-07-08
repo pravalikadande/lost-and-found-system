108693e13f1f4b8707b28a823a329458d2459650
pravalikadande <dandeprvalika160@gmail.com>
2026-07-08 12:12:36 +0530

    update lostItems

--- server/controllers/lostController.js
+++ server/controllers/lostController.js
@@ -98,6 +98,34 @@ const getMyLostItems = async (req, res) => {
   }
 };
 
+const deleteLostItem = async (req, res) => {
+  try {
+    const item = await Lost.findById(req.params.id);
+
+    if (!item) {
+      return res.status(404).json({
+        message: "Item not found",
+      });
+    }
+
+    if (item.user.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        message: "Only the item owner can delete this item",
+      });
+    }
+
+    await item.deleteOne();
+
+    res.status(200).json({
+      message: "Lost Item Deleted Successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   addLostItem,
   getLostItems,
